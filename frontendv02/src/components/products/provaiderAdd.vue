a5e8f4042afc46ce9fe4d0b79fbff8953f41c16d
siipi sistemas <eljros999xxx@gmail.com>
2022-11-07 15:19:35 -0400

    test

--- frontendv02/src/components/products/provaiderAdd.vue
+++ frontendv02/src/components/products/provaiderAdd.vue
@@ -1,89 +1,65 @@
 <template>
   <q-page  padding>
-    <div class="row q-col-gutter-sm justify-center">
-      <div class="col-lg-7 col-md-7 col-sm-12 col-xs-12">
-        <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-lg q-mt-md">
-          <div class="row">
-            <div class="col-12">
-              <q-item>
-                <q-input
-                  dense
-                  outlined
-                  class="full-width"
-                  v-model="name"
-                  type="text"
-                  label="Nombre del Proveedor"
-                />
-              </q-item>
-            </div>
+    <div class="row justify-center">
+        <div class="col-xs-10 col-sm-6 col-md-5 col-lg-5 col-xl-2">
+           
+            <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-lg q-mt-md">
+                <q-input filled v-model="name" label="Nombre del Producto *" hint="Mínimo 3, Máximo 30 caracteres"
+                    lazy-rules :rules="[
+                      (val) =>
+                        (val && val.length > 2) || 'Nombre(s), Mínimo 3 caracteres',
+                      (val) => val.length < 31 || 'Nombre(s), Máximo 30 caracteres',
+                    ]" />
 
-            <div class="col-12">
-              <q-item>
-                <q-input
-                  dense
-                  outlined
-                  class="full-width"
-                  v-model="email"
-                  type="email"
-                  label="Correo del Proveedor"
-                />
-              </q-item>
-            </div>
+                <q-input filled v-model="shelf" type="text" label="Estante"> <template v-slot:prepend>
+                        <q-icon name="scatter_plot" />
+                    </template></q-input>
 
-            <div class="col-12">
-              <q-item>
-                <q-input
-                  dense
-                  outlined
-                  class="full-width"
-                  v-model="phone"
-                  type="tel"
-                  label="Telefono Proveedor"
-                />
-              </q-item>
-            </div>
+                <q-input filled v-model="hallway" type="text" label="Pasillo"><template v-slot:prepend>
+                        <q-icon name="near_me" />
+                    </template> </q-input>
 
-            <div class="col-12">
-              <q-item>
-                <q-input
-                  dense
-                  outlined
-                  class="full-width"
-                  v-model="address"
-                  type="text"
-                  label="Direccion del Proveedor"
-                />
-              </q-item>
-            </div>
-            <div class="col-12">
-              <q-item>
-                <q-input
-                  dense
-                  outlined
-                  class="full-width"
-                  v-model="city"
-                  type="text"
-                  label="Ciudad"
-                />
-              </q-item>
-            </div>
+                <q-input filled v-model="minStock" type="number" label="Stock Minimo"><template v-slot:prepend>
+                        <q-icon name="fa-solid fa-arrow-trend-up" />
+                    </template>
+                </q-input>
+                <q-input filled v-model="maxStock" type="number" label="Stock Maximo"> <template v-slot:prepend>
+                        <q-icon name="fa-solid fa-arrow-trend-down" />
+                    </template>
+                </q-input>
+                <q-input filled v-model="dateOfExpiration" type="date" hint="Fecha de Vencimiento" />
 
-              
-          </div>
-          <div >
-            <q-btn label="agregar" type="submit" color="primary" />
-            <q-btn
-              label="Cancelar"
-              type="reset"
-              color="primary"
-              flat
-              class="q-ml-sm"
-            />
-          </div>
-         
-        </q-form>
-      </div>
-     
+                <q-input filled v-model="price" type="number" prefix="Bs" label="Precio" lazy-rules :rules="[
+                  (val) => (val !== null && val !== '') || 'Ingrese un precio',
+                  (val) => (val > 0 && val < 99999999) || 'Ingrese un precio válido',
+                ]">
+                    <template v-slot:prepend>
+                        <q-icon name="fa-solid fa-money-bill-1-wave" />
+                    </template>
+                </q-input>
+
+                <q-input filled v-model="dayMargin" type="number" label="Margen de Dias de Vencimiento" />
+
+                <q-select filled v-model="status" label="Estado del Producto *" :options="[
+                  { label: 'active', value: 'active' },
+                  { label: 'inactive', value: 'inactive' },
+                  { label: 'pending', value: 'pending' },
+                  { label: 'blocked', value: 'blocked' },
+                  { label: 'deleted', value: 'deleted' },
+                ]" />
+
+                <q-file label-color="orange" filled use-chips v-model="image" label="Imagen del Producto">
+                    <template v-slot:append>
+                        <q-icon name="cloud_upload" />
+                    </template>
+                </q-file>
+
+                <div>
+                    <q-btn label="Guardar" type="submit" color="orange" />
+                    <q-btn label="Cancelar" type="reset" color="grey-6" class="q-ml-sm" />
+                </div>
+            </q-form>
+        </div>
     </div>
   </q-page>
  
